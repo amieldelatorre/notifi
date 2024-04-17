4382fbb8a699243f2e75be749531b8873b981f6d
amieldelatorre <amieljames.delatorre@gmail.com>
2024-04-17 21:05:10 +1200

    Fix test that was dependent on time.Now()

--- service/user/user_test.go
+++ service/user/user_test.go
@@ -96,7 +96,9 @@ func TestGetUserById(t *testing.T) {
 			t.Fatalf("test case userId %d, expected response errors %+v, got %+v", tc.UserId, tc.Response.Errors, actualResponse.Errors)
 		}
 
-		if !reflect.DeepEqual(actualResponse.User, tc.Response.User) {
+		if actualResponse.User != nil && tc.Response.User != nil && (actualResponse.User.Id != tc.Response.User.Id || actualResponse.User.Email != tc.Response.User.Email ||
+			actualResponse.User.FirstName != tc.Response.User.FirstName || actualResponse.User.LastName != tc.Response.User.LastName ||
+			actualResponse.User.Password != tc.Response.User.Password) {
 			t.Fatalf("test case userId %d, expected response user %+v, got %+v", tc.UserId, tc.Response.User, actualResponse.User)
 		}
 	}
