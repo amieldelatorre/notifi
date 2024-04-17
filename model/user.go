127b294acd8568a8ce1eaecc3d2f5d4d5d8e8a83
amieldelatorre <amieljames.delatorre@gmail.com>
2024-04-17 20:40:34 +1200

    Don't return user's password when getting a user

--- model/user.go
+++ model/user.go
@@ -3,11 +3,11 @@ package model // import "github.com/amieldelatorre/notifi/model"
 import "time"
 
 type User struct {
-	Id              int
-	Email           string
-	FirstName       string
-	LastName        string
-	Password        string
-	DatetimeCreated time.Time
-	DatetimeUpdated time.Time
+	Id              int       `json:"id"`
+	Email           string    `json:"email"`
+	FirstName       string    `json:"firstName"`
+	LastName        string    `json:"lastName"`
+	Password        string    `json:"-"`
+	DatetimeCreated time.Time `json:"datetimeCreated"`
+	DatetimeUpdated time.Time `json:"datetimeUpdated"`
 }
