23ae9153e4fd0374223449f1974a68aa645f68bb
amieldelatorre <amieljames.delatorre@gmail.com>
2024-04-17 18:19:35 +1200

    Remove UserDb  that was not used

--- repository/user/userprovider.go
+++ repository/user/userprovider.go
@@ -9,11 +9,3 @@ import (
 type UserProvider interface {
 	GetUserById(ctx context.Context, id int) (model.User, error)
 }
-
-type UserDb struct {
-	Provider UserProvider
-}
-
-func NewUserDb(provider UserProvider) *UserDb {
-	return &UserDb{Provider: provider}
-}
