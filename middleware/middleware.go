d128f056dc1754fc868478613ad1c4c5ed252a72
amieldelatorre <amieljames.delatorre@gmail.com>
2024-04-22 21:12:10 +1200

    Moving files around

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,7 +1,11 @@
 package middleware // import "github.com/amieldelatorre/notifi/middleware"
 
 import (
+	"context"
 	"log/slog"
+	"net/http"
+
+	"github.com/google/uuid"
 )
 
 type Middleware struct {
@@ -10,4 +14,42 @@ type Middleware struct {
 
 func New(logger *slog.Logger) Middleware {
 	return Middleware{Logger: logger}
+}
+
+func (m *Middleware) AddRequestId(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		m.Logger.Debug("Adding a request id to incoming request")
+		id, err := uuid.NewV7()
+		if err != nil {
+			m.Logger.Error("Problem generating uuid: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		m.Logger.Debug("Request id generated and added to context", "requestId", id.String())
+
+		ctx := context.WithValue(r.Context(), RequestIdName, id.String())
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func (m *Middleware) RequireJwtToken(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestId := r.Context().Value(RequestIdName)
+		m.Logger.Debug("Checking if request has the Authorization Bearer token", "requestId", requestId)
+
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		// Validate token
+		// If not valid, return Unauthorized
+		// If valid set the UserId in context
+		ctx := r.Context()
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
--- middleware/middleware_test.go
+++ middleware/middleware_test.go
@@ -3,6 +3,9 @@ package middleware
 import (
 	"io"
 	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
 	"github.com/amieldelatorre/notifi/logger"
 )
@@ -10,4 +13,25 @@ import (
 func GetMockMiddleware() Middleware {
 	logger := logger.New(io.Discard, slog.LevelWarn)
 	return Middleware{Logger: logger}
+}
+
+func TestAddRequestId(t *testing.T) {
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		val := r.Context().Value(RequestIdName)
+		if val == nil {
+			t.Fatalf("'%s' is missing", RequestIdName)
+		}
+
+		value, ok := val.(string)
+		if !ok {
+			t.Fatalf("Value of '%s', %+v, is not a string", RequestIdName, value)
+		}
+	})
+
+	m := GetMockMiddleware()
+	handler := m.AddRequestId(nextHandler)
+	request := httptest.NewRequest("GET", "http://example.invalid", nil)
+
+	handler.ServeHTTP(httptest.NewRecorder(), request)
+
 }
